15a26f1f741266a0e23a42ec8d77872409591ff3
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 15:10:38 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./src/fs/files/');
+
 const rename = async () => {
-    // Write your code here 
+    fs.access(
+        path.join(__dirname, 'files', 'properFilename.md'),
+        err => {
+            if (!err) throw new Error('FS operation failed');
+        }
+    )
+    fs.rename(
+        path.join(__dirname, 'files', 'wrongFilename.txt'),
+        path.join(__dirname, 'files', 'properFilename.md'),
+        err => {
+            if (err) throw new Error('FS operation failed');
+        }
+    );
 };
 
 await rename();
