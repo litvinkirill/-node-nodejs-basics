f531ab9261893d4f07ee48a5db0ca0b87b65492a
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 14:41:14 +0300

    feat: add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./src/fs/files/');
+
 const remove = async () => {
-    // Write your code here 
+    fs.unlink(
+        path.join(__dirname, 'files', 'fileToRemove.txt'), 
+        (err) => {
+            if(err) throw new Error('FS operation failed');
+        }
+    );
 };
 
 await remove();
