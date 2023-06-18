c7d11485beb21000af48c042adcdf7d9997f5a57
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 15:32:56 +0300

    feat: add streams:read funcion

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'));
+    readableStream.pipe(stdout);
 };
 
 await read();
