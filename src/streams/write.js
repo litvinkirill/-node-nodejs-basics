dca95883e70ce23ac9b6a15cc0083af93c4a37a2
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 15:38:52 +0300

    feat: add streams:write finction

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdin } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const output = fs.createWriteStream(path.join(__dirname, 'files/fileToWrite.txt'));
+    stdin.pipe(output);
 };
 
 await write();
