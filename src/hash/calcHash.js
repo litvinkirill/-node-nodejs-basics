4fbf9fc49017330155e74a44158a7299eeccd405
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 15:15:00 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file = await fs.readFile(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    const hash = crypto.createHash('sha256');
+    hash.update(file);
+    const hex = hash.digest('hex');
+    console.log(hex);
+    return hex;
 };
 
 await calculateHash();
