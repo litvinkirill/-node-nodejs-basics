3d3981519751ff6b081d25d965534f1ef43497ee
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 14:38:19 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./src/fs/files/');
+
 const copy = async () => {
-    // Write your code here 
+    if (!fs.existsSync('./src/fs/files/')) {
+        throw new Error('FS operation failed');
+    }
+    fs.mkdir(
+        path.join(__dirname, 'files_copy'),
+        (err) => {
+            if(err) throw new Error('FS operation failed');
+        }
+    );
+    fs.cp(
+        path.join(__dirname, 'files'),
+        path.join(__dirname, 'files_copy'),
+        { recursive: true },
+        (err) => {
+            if(err) throw new Error('FS operation failed');
+        }
+    )
 };
 
 await copy();
