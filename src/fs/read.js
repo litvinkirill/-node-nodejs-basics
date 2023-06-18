40e8b8a2c7ab246d26c2d8a53d004c13e1d1e86b
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 15:07:43 +0300

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./src/fs/files/');
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(
+        path.join(__dirname, 'files', 'fileToRead.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) throw new Error('FS operation failed');
+            console.log(data);
+        }
+    )
 };
 
 await read();
