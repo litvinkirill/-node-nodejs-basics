d8755c5a8995e2bcaba0e3a3b1d60131eafa2072
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 15:18:32 +0300

    feat: add parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.map((_, i) => {
+            if (i % 2) return;
+            console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+        });
 };
 
 parseArgs();
