7454e5427304d22dc2c253bf1f764dfd0d432f78
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-18 14:32:01 +0300

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./src/fs/files/');
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(
+        path.join(__dirname, 'files', 'fresh.txt'),
+        'I am fresh and young',
+        { flag: 'wx' },
+        (err) => {
+            if (err) throw new Error('FS operation failed');
+        }
+    )
 };
 
 await create();
