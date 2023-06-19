97ad7cd5b0911a49a5b82cf63aa8fbbfbbf244ac
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-19 22:33:00 +0300

    fix: path to files

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname('./src/fs/files/');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const remove = async () => {
     fs.unlink(
-        path.join(__dirname, 'files', 'fileToRemove.txt'), 
+        path.join(__dirname, 'files', 'fileToRemove.txt'),
         (err) => {
             if(err) throw new Error('FS operation failed');
         }
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname('./src/fs/files/');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const list = async () => {
     fs.readdir(
