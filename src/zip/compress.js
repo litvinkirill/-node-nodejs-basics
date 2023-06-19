82c7892d83af7c28d5e6400c93d73a6420c52acb
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-06-19 22:17:48 +0300

    feat: add compress and decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const zip = zlib.createGzip();
+    const src = fs.createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+    const output = fs.createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+
+    src.pipe(zip).pipe(output);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const unZip = zlib.createGunzip();
+    const src = fs.createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+    const output = fs.createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+
+    src.pipe(unZip).pipe(output);
 };
 
 await decompress();
